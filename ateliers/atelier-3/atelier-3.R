0795b8f0a3fb6ea9162020943f71f78419f6842d
Philippe Leblanc <philippe.leblanc.18@ulaval.ca>
2025-01-24 09:57:29 -0500

    ajouter fin atelier 3

--- ateliers/atelier-3/atelier-3.R
+++ ateliers/atelier-3/atelier-3.R
@@ -75,6 +75,10 @@ f2 <- dnbinom(x, size = 0.4, prob = 1/11)
 f3 <- dnbinom(x, size = 4, prob = 1/2)
 f4 <- 0.8 * dpois(x, lambda = 2) + 0.2 * dpois(x, lambda = 12)
 
+##
+## Méthode 1: Utiliser des fonctions
+##
+
 # Créer une liste contenant les fonctions de masse de probabilité
 f_list <- list(f1, f2, f3, f4)
 
@@ -88,6 +92,10 @@ sapply(f_list, function(f) sum((x - sum(x * f))^2 * f))
 SL <- function(d, f) sum(pmax(x - d, 0) * f)
 sapply(f_list, function(f) SL(5, f))
 
+## Espérance tronquée
+EspTr <- function(d, f) sum(x * f * I(x > d))
+sapply(f_list, function(f) EspTr(5, f))
+
 ## Value at Risk
 VaR <- function(k, f) x[min(which(cumsum(f) >= k))]
 sapply(f_list, function(f) VaR(0.99, f))
@@ -100,6 +108,34 @@ sapply(f_list, function(f) TVaR(0.99, f))
 RVaR <- function(k1, k2, f)
     ((1 - k1) * TVaR(k1, f) - (1 - k2) * TVaR(k2, f))/(k2 - k1)
 sapply(f_list, function(f) RVaR(0.99, 0.995, f))
+
+##
+## Méthode 2: Manuellement remplacer f, d et k par leurs valeurs
+##
+
+## Espérances
+sum(x * f)
+
+## Variances
+sum((x - sum(x * f))^2 * f)
+
+## Stop-loss
+SL <- sum(pmax(x - d, 0) * f)
+
+## Espérance tronquée
+sum(x * f * I(x > d))
+
+## Value at Risk
+Fx <- cumsum(f)
+x[min(which(Fx >= k))]
+
+## Tail Value at Risk
+# On ne peut pas calculer avec les fonctions, il faut remplacer les valeurs.
+SL(VaR(k))/(1 - k) + VaR(k)
+
+## Range Value at Risk
+# On ne peut pas calculer avec les fonctions, il faut remplacer les valeurs.
+((1 - k1) * TVaR(k1) - (1 - k2) * TVaR(k2))/(k2 - k1)
 
 
 ###
@@ -174,3 +210,68 @@ TVaR(0.99)
 ## Range Value at Risk
 RVaR <- function(k1, k2) ((1 - k1) * TVaR(k1) - (1 - k2) * TVaR(k2))/(k2 - k1)
 RVaR(0.9, 0.99)
+
+
+###
+### Loi Weibull
+###
+
+rm(list = ls())
+
+## Paramétrisation en R
+# tau --> shape
+# beta --> 1 / scale
+?pweibull
+
+be <- 2
+tau <- c(0.5, 2)
+
+## Fonctions de densité
+fx <- function(x, tau, be) be * tau * (be * x)^(tau - 1) * exp(-(be * x)^tau)
+fx(5, tau[1], be)
+dweibull(5, tau[1], 1/be)
+
+# Courbes de densité
+curve(dweibull(x, tau[1], 1/be), lwd = 3, col = 'blue', xlim = c(0, 10),
+      main = 'Densités de la loi Weibull', ylab = 'f(x)', lty = 1)
+curve(dweibull(x, tau[2], 1/be), lwd = 3, col = 'red', add = TRUE, lty = 1)
+legend(x = 'topright', legend = c('tau = 2', 'tau = 0.5'),
+       col = c('blue', 'red'), cex = 1.5, lwd = 3, lty = 1)
+
+## Fonctions de répartition
+Fx <- function(x, be, tau) 1 - exp(-(be * x)^tau)
+Fx(5, be, tau[1])
+pweibull(5, tau[1], 1/be)
+
+# Courbes de répartition
+curve(pweibull(x, tau[1], 1/be), lwd = 3, col = 'blue', xlim = c(0, 10),
+      main = 'Répartitions de la loi Weibull', ylab = 'f(x)')
+curve(pweibull(x, tau[2], 1/be), lwd = 3, col = 'red', add = TRUE, lty = 1)
+legend(x = 'bottomright', legend = c('tau = 2', 'tau = 0.5'),
+       col = c('blue', 'red'), cex = 1.5, lwd = 3, lty = 1)
+
+## Fonction quantile
+quantile <- function(u, tau, be) ((-log(1 - u))^(1/tau))/be
+qweibull(0.5, tau[1], 1/be)
+quantile(0.5, tau[1], be)
+
+# Courbes de quantile
+curve(qweibull(x, tau[1], 1/be), lwd = 3, col = 'blue', xlim = c(0, 1),
+      main = 'Fonctions quantiles', ylab = 'VaR(X)', lty = 1, xlab = 'kappa')
+curve(qweibull(x, tau[2], 1/be), lwd = 3, col = 'red', add = TRUE, lty = 1)
+legend(x = 'bottomright', legend = c('tau = 2', 'tau = 0.5'),
+       col = c('blue', 'red'), cex = 1.5, lwd = 3, lty = 1)
+
+## Tail Value at Risk
+VaR <- function(u, tau, be) ((-log(1 - u))^(1/tau))/be
+TVaR <- function(u, tau, be) 1/(be * (1 - u)) * gamma(1 + 1/tau) * pgamma(
+      -log(1 - u), 1 + 1/tau, 1, lower = FALSE
+)
+TVaR(0.95, tau[1], be)
+
+# Courbes de TVaR
+curve(sapply(x, TVaR, tau[1], be), lwd = 3, col = 'blue', xlim = c(0, 1),
+      main = 'TVaR de la Weibull', ylab = 'TVaR(X)', lty = 1, xlab = 'kappa')
+curve(sapply(x, TVaR, tau[2], be), lwd = 3, col = 'red', add = TRUE, lty = 1)
+legend(x = 'topleft', legend = c('tau = 2', 'tau = 0.5'),
+       col = c('blue', 'red'), cex = 1.5, lwd = 3, lty = 1)
